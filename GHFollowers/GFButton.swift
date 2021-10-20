56d2f6e920cb601c51b8b906eb6090bf32494b83
femialiu <femialiu713@gmail.com>
2021-10-20 21:14:47 +0100

    created custom UIButton and UITextField

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,34 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Femi Aliu on 20/10/2021.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
