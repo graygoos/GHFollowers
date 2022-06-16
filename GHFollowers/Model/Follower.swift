654aa908883e916dc9ce444f1565e308c92d4a81
femialiu <femialiu713@gmail.com>
2022-06-16 14:37:53 +0100

    began planning network calls - created User and Follower objects

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Femi Aliu on 16/06/2022.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
