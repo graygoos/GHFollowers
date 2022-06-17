da8a47f4ef01849372537b0f73c7060a8fee10c5
femialiu <femialiu713@gmail.com>
2022-06-17 12:19:25 +0100

    created Follower cell and avatarImageView

---
+++ GHFollowers/Custom views/GFAvatarImageView.swift
@@ -0,0 +1,31 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Femi Aliu on 17/06/2022.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+
+}
