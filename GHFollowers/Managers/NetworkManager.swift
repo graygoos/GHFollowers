3fa91dbd5703956ab4d8b2051a8fde99b440a2ba
femialiu <femialiu713@gmail.com>
2022-06-22 09:37:24 +0100

    downloading and caching avatar images

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Femi Aliu on 16/06/2022.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
