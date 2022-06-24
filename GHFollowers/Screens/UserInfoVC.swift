3ed653963f6fda7b8cf31c2c17dfa24471e56abd
femialiu <femialiu713@gmail.com>
2022-06-24 12:53:56 +0100

    created modal presentation for UserInfoVC and passing data

---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,27 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Femi Aliu on 24/06/2022.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        print(username!)
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+}
