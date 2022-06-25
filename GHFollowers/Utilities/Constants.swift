dfbfeb052820d7e7fa9dd68cf2c7f9c92c876a4a
femialiu <femialiu713@gmail.com>
2022-06-25 18:34:34 +0100

    began creating child view containers for the UserInfo view controller

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Femi Aliu on 25/06/2022.
+//
+
+import Foundation
+
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
