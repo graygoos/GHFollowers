d045522bad46603080b60287279158c4059772d1
femialiu <femialiu713@gmail.com>
2022-06-27 12:24:58 +0100

    created subclasses for ItemInfoVC

---
+++ GHFollowers/Custom views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollowers
+//
+//  Created by Femi Aliu on 27/06/2022.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
---
+++ GHFollowers/Custom views/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by Femi Aliu on 27/06/2022.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
