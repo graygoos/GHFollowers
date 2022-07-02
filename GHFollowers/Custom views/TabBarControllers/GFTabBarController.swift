13789952960996331dd3cc5237ef44cba5d2b4dc
femialiu <femialiu713@gmail.com>
2022-07-02 12:34:13 +0100

    optimisation - refactored tabbar controller from scene delegate file

---
+++ GHFollowers/Custom views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  GFTabBarController.swift
+//  GHFollowers
+//
+//  Created by Femi Aliu on 02/07/2022.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+        
+    }
+    
+    
+    func createSearchNC() -> UINavigationController {
+        
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    
+    func createFavoritesNC() -> UINavigationController {
+        
+        let favoritesListVC = FavoritesListVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+}
