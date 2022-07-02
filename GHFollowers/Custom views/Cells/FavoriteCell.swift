ea168d509cc5ee82269ed587668909d9178f8d28
femialiu <femialiu713@gmail.com>
2022-07-02 09:32:07 +0100

    completed Favorites view controller

--- GHFollowers/Custom views/Cells/FavoriteCell.swift
+++ GHFollowers/Custom views/Cells/FavoriteCell.swift
@@ -42,7 +42,7 @@ class FavoriteCell: UITableViewCell {
             
             usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
-            avatarImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
             usernameLabel.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
