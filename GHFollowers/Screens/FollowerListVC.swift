dd392e49360e2e5da3a15727fcae318fb895f19f
femialiu <femialiu713@gmail.com>
2022-07-06 07:00:22 +0100

    created GFDataLoadingVC superclass

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -11,7 +11,7 @@ protocol FollowerListVCDelegate: AnyObject {
     func didRequestFollowers(for username: String)
 }
 
-class FollowerListVC: UIViewController {
+class FollowerListVC: GFDataLoadingVC {
     
     enum Section { case main }
     
